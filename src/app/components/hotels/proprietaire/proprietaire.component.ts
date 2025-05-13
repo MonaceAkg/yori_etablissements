1930e8adcb029fafa1e5e05bd764ef8aeffeac07
devIgf <igfdev0@gmail.com>
2025-05-13 10:23:01 +0000

    correction de l'ajout du propriétaire

--- src/app/components/hotels/proprietaire/proprietaire.component.ts
+++ src/app/components/hotels/proprietaire/proprietaire.component.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { MatDivider } from '@angular/material/divider';
 
 
 /** Error when invalid control is dirty, touched, or submitted. */
@@ -18,11 +16,6 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   selector: 'app-proprietaire',
   standalone: true,
   imports: [
-    MatCard,
-    MatCardHeader,
-    MatCardTitle,
-    MatCardContent,
-    MatDivider,
     FormsModule,
     ReactiveFormsModule,
     CommonModule
@@ -30,7 +23,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './proprietaire.component.html',
   styleUrl: './proprietaire.component.css'
 })
-export class ProprietaireComponent implements OnInit{
+export class ProprietaireComponent implements OnInit {
   @ViewChild('phoneInput', { static: false }) phoneInputElement!: ElementRef; // Utilisation de ViewChild
   options: string[] = ["Je certifie que l'activité d'hébergement est légale et détient toutes les autorisations requises, pouvant être présentées sur demande. Mbolo.com B.V. se réserve le droit de vérifier toutes les informations fournies lors de cette inscription.", "J'ai lu et j'accepte les Conditions Génerales de Prestation."];
 
@@ -44,7 +37,7 @@ export class ProprietaireComponent implements OnInit{
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   matcher = new MyErrorStateMatcher();
 
@@ -58,7 +51,57 @@ export class ProprietaireComponent implements OnInit{
     }
   }
 
+  images: string[] = [];
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.images.push(e.target.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+  onUploadClick(): void {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+
+        reader.onload = (e: any) => {
+          this.images.push(e.target.result);
+        };
+
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  removeImage(image: string): void {
+    this.images = this.images.filter(img => img !== image);
+  }
+
+
 }
+
+
 function intlTelInput(nativeElement: any, arg1: { initialCountry: string; separateDialCode: boolean; utilsScript: string; }) {
   throw new Error('Function not implemented.');
 }
