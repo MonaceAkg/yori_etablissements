6d63b8c1630b482d12de587f4906028ffd7b472e
devIgf <igfdev0@gmail.com>
2025-05-20 15:40:47 +0000

    fin de la page 'les inclus'

--- src/app/components/Tourisme/progression-tour1/info-importantes/info-importantes.component.ts
+++ src/app/components/Tourisme/progression-tour1/info-importantes/info-importantes.component.ts
@@ -1,15 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-info-importantes',
   standalone: true,
   imports: [
-    MatCardHeader,
-    MatCardContent,
-    MatCardTitle ,
-    CommonModule   
+    CommonModule
   ],
   templateUrl: './info-importantes.component.html',
   styleUrl: './info-importantes.component.css'
@@ -19,17 +15,16 @@ export class InfoImportantesComponent {
     { value: '', editable: true },
     { value: '', editable: true },
     { value: '', editable: true },
-];
+  ];
 
-addInput() {
+  addInput() {
     this.inputs.push({ value: '', editable: true });
-}
+  }
 
-updateInput(index: number, event: Event) {
+  updateInput(index: number, event: Event) {
     const target = event.target as HTMLDivElement; // Cast pour HTMLDivElement
     this.inputs[index].value = target.textContent || ''; // Met à jour la valeur
-}
+  }
 
 }
 
- 
